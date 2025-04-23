1a014d7f7c058ba8f7abf55ad2f93493f68a14d1
afrad07 <afradafu3@gmail.com>
2025-04-23 19:43:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,32 @@
 let PlayerScore = 0;
 let ComputerScore = 0;
 
+
+
 function playGame(playerChoice) {
     const choices = ["rock", "paper", "scissors"];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    document.getElementById("result").innerHTML = " ";
 
     document.getElementById("playerChoice").innerHTML = `${playerChoice}`;
     document.getElementById("computerChoice").innerHTML = `${computerChoice}`;
 
     if (playerChoice === computerChoice) {
-        document.getElementById("result").innerHTML = "It's a tie!";
+        document.getElementById("result").innerHTML = "It's a tie! ⚖️";
         document.getElementById("result").style.color = "black";
     } else if (
         (playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "paper" && computerChoice === "rock") ||
         (playerChoice === "scissors" && computerChoice === "paper")
     ) {
-        document.getElementById("result").innerHTML = "You win!";
+        document.getElementById("result").innerHTML = "You win! 🏆";
         document.getElementById("result").style.color = "green";
         PlayerScore = PlayerScore + 1;
         document.getElementById("playerScore").innerHTML = PlayerScore;
         
         
     } else {
-        document.getElementById("result").innerHTML = "You lose!";
+        document.getElementById("result").innerHTML = "You lose! 🖕";
         document.getElementById("result").style.color = "red";
         ComputerScore = ComputerScore + 1;
         document.getElementById("computerScore").innerHTML = ComputerScore;
